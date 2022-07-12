f972d6bf3630f90854e03eee5b9296a71f12028c
unknown <shroukalkalla1997@gmail.com>
2022-07-12 12:57:47 +0200

    Endpoint Testing

--- Lesson04/src/index.ts
+++ Lesson04/src/index.ts
@@ -27,55 +27,73 @@
 //=====================================================================//
 
 /** Use Axios to get data from restcountries api */
-import axios from 'axios';
+// import axios from 'axios';
 
-/** Use the free API https://restcountries.eu/
- * You will use the following endpoints
- * https://restcountries.eu/rest/v2/name/{name} for countries by name
- * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
- */
+// /** Use the free API https://restcountries.eu/
+//  * You will use the following endpoints
+//  * https://restcountries.eu/rest/v2/name/{name} for countries by name
+//  * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
+//  */
 
-/** Create getCountry Function here */
-async function getCountry(name: string){
-    const getApi = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
-    const data = getApi.data;
-    return {
-        capital : data.capital,
-        region: data.region,
-        numericCode: data.numericCode
-    }
-}
+// /** Create getCountry Function here */
+// async function getCountry(name: string){
+//     const getApi = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
+//     const data = getApi.data;
+//     return {
+//         capital : data.capital,
+//         region: data.region,
+//         numericCode: data.numericCode
+//     }
+// }
 
-/** Create a test for this getRegion function */
-async function getRegionCountries(regionalbloc: string) {
-  const getApi = await axios(
-    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
-  );
-  const data = getApi.data;
-  const countries = [];
-  for (let i = 0; i < data.length; i++) {
-    countries.push(data[i].name);
-  }
-  return countries;
-}
+// /** Create a test for this getRegion function */
+// async function getRegionCountries(regionalbloc: string) {
+//   const getApi = await axios(
+//     `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
+//   );
+//   const data = getApi.data;
+//   const countries = [];
+//   for (let i = 0; i < data.length; i++) {
+//     countries.push(data[i].name);
+//   }
+//   return countries;
+// }
 
-/** Create getRegionCapitals function here */
-async function getRegionCapitals(regionalbloc: string){
-    const countryEndpoints = await getRegionCountries(regionalbloc);
-    const promises = countryEndpoints.map((endpoint: any) => 
-       axios(`https://restcountries.eu/rest/v2/name/${endpoint}`
-    ));
-    const arr = await Promise.all(promises);
-    const capitals = [];
-    for (let i=0; i < arr.length; i++) {
-        capitals.push(arr[i].data[0].capital);
-    }
-    return capitals;
-}
+// /** Create getRegionCapitals function here */
+// async function getRegionCapitals(regionalbloc: string){
+//     const countryEndpoints = await getRegionCountries(regionalbloc);
+//     const promises = countryEndpoints.map((endpoint: any) => 
+//        axios(`https://restcountries.eu/rest/v2/name/${endpoint}`
+//     ));
+//     const arr = await Promise.all(promises);
+//     const capitals = [];
+//     for (let i=0; i < arr.length; i++) {
+//         capitals.push(arr[i].data[0].capital);
+//     }
+//     return capitals;
+// }
 
 
-export default {
-  getCountry,
-  getRegionCountries,
-  getRegionCapitals
-};
+// export default {
+//   getCountry,
+//   getRegionCountries,
+//   getRegionCapitals
+// };
+
+//=====================================================================//
+
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+//set endpoint:
+app.get('/api', (req, res) => {
+    res.send("Hello World");
+});
+
+//check for port to avoid already in use error testing:
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
+
+export default app;
+
--- Lesson04/src/tests/indexSpec.ts
+++ Lesson04/src/tests/indexSpec.ts
@@ -43,29 +43,44 @@
 
 //=====================================================================//
 
-import countries from "../index";
+// import countries from "../index";
 
-it("should get basic data on the country canada", async () => {
-  const data = await countries.getCountry('canada');
-  expect(data).toEqual({
-    capital: 'Ottawa',
-    region: 'Americas',
-    numericCode: '124'
-  });
-});
+// it("should get basic data on the country canada", async () => {
+//   const data = await countries.getCountry('canada');
+//   expect(data).toEqual({
+//     capital: 'Ottawa',
+//     region: 'Americas',
+//     numericCode: '124'
+//   });
+// });
 
 /** Add test for getRegionCountries function here */
-it("should get the countries in the region NAFTA", async () => {
-  const data = await countries.getRegionCountries('nafta');
-  expect(data).toEqual([
-      'Canada', 'Mexico', 'United States of America'
-  ]);
-});
+// it("should get the countries in the region NAFTA", async () => {
+//   const data = await countries.getRegionCountries('nafta');
+//   expect(data).toEqual([
+//       'Canada', 'Mexico', 'United States of America'
+//   ]);
+// });
 
 
-it("should get capitals of NAFTA countries", async () => {
-  const data = await countries.getRegionCapitals('nafta');
-  expect(data).toEqual([
-    'Ottawa', 'Mexico City', 'Washington, D.C.'
-  ]);
-});
+// it("should get capitals of NAFTA countries", async () => {
+//   const data = await countries.getRegionCapitals('nafta');
+//   expect(data).toEqual([
+//     'Ottawa', 'Mexico City', 'Washington, D.C.'
+//   ]);
+// });
+
+//=====================================================================//
+
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async (done) => {
+        const response = await request.get('/api');
+        expect(response.status).toBe(200);
+        done();
+    }
+)});
+
